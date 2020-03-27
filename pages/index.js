ff92188d05edc412cd660c93363087bc226866f9
wtto <wtto163@163.com>
2020-03-27 11:34:33 +0800

    prerender md file from github

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
 import marked from "marked";
 import hljs from "highlight.js";
+import fetch from 'node-fetch'
 import Header from "components/Header";
 import Footer from "components/Footer";
-import md from "components/md/index.md";
+import "./index.scss";
 
 marked.setOptions({
   highlight: code => hljs.highlightAuto(code).value
 });
 
-import "./index.scss";
 
-const Home = () => (
+const Home = ({ content }) => (
   <div className="container">
     <Header />
 
     <main
       className="markdown"
-      dangerouslySetInnerHTML={{ __html: marked(md) }}
+      dangerouslySetInnerHTML={{ __html: marked(content) }}
     ></main>
 
     <Footer />
   </div>
 );
+
+export async function getStaticProps(context) {
+  const res = await fetch('https://raw.githubusercontent.com/wtto00/badge/master/README.md')
+  const content = await res.text()
+  return {
+    props: { content },
+  }
+}
 
 export default Home;
